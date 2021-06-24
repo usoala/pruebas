f93140841f4f27e1b0c0fbfdd08427a21c358f1e
Usoa <usoa.larrarte@gmail.com>
2021-06-24 08:59:50 +0200

    pruebas con gitignore

---
+++ PruebaParaGitIgnoreJAVA/src/module-info.java
@@ -0,0 +1,2 @@
+module PruebaParaGitIgnoreJAVA {
+}
---
+++ PruebaParaGitIgnoreSPRING/src/main/java/com/example/demo/PruebaParaGitIgnoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaParaGitIgnoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaParaGitIgnoreSpringApplication.class, args);
+	}
+
+}
